d5b2e5429e4f312cc06bd74014cb0078f697b903
Lake3L <danille506@gmail.com>
2025-09-03 13:52:03 +0300

    Create import os.py

---
+++ import os.py
@@ -0,0 +1,43 @@
+import os
+import platform
+
+class Shell:
+    def __init__(self): #
+        self.running = True
+
+    def get_promt(self):
+
+        username = os.getlogin()
+        hostname = platform.node()
+        invite = f"{username}@(hostname):~$: " #2 этап
+        return invite
+
+    def parser_input(self, input_string): #3 этап
+        parser = input_string.strip().split()
+        command = parser[0]
+        args = parser[1:]
+        return command, args
+    
+    def execute_command(self, command, args):
+        if command == "exit":
+            self.running = False
+            print("Выход из эмулятора.")
+        
+        elif command == "ls":
+            print(f"Команда: ls, Аргументы: {args}")
+
+        elif command == "cd":
+            print(f"Команда: cd, Аргументы: {args}")
+
+        else: print(f"Неизвестная команда {command}")
+
+    def run(self):
+        while self.running:
+            user_input = input(self.get_promt()) #Ввод
+            command, args = self.parser_input(user_input) #Парсинг ввода
+
+            self.execute_command(command, args) #запуск обработчика комманд
+
+if __name__ == "__main__":
+    shell = Shell()
+    shell.run()
